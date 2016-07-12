9fb91096486e31d5a91e53cfb8b6c31e0562b460
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-07-12 16:42:35 -0700

    update R script to fix #1

--- pinfestans_ia_and_msn.R
+++ pinfestans_ia_and_msn.R
@@ -1,9 +1,11 @@
-## ----packages, message = FALSE-------------------------------------------
+# R packages
+# Note: poppr spits out a lot of startup messages, probably want to suppress
+# this when converting to RMarkdown
 library('poppr')
 library('ggplot2')
 library('RColorBrewer')
 
-## ----data_setup----------------------------------------------------------
+# data setup
 pinf <- read.genalex("pinfestans_mx_sa.csv", ploidy = 3)
 splitStrata(pinf) <- ~Continent/Country
 setPop(pinf) <- ~Continent
@@ -11,7 +13,9 @@ pinfreps <- c(Pi02 = 2, D13 = 2, Pi33 = 6, Pi04 = 2, Pi4B = 2, Pi16 = 2,
               G11 = 2, Pi56 = 2, Pi63 = 3, Pi70 = 3, Pi89 = 2)
 pinfreps <- fix_replen(pinf, pinfreps)
 
-## ----ia_analysis, fig.show='hide', results = 'asis'----------------------
+# ia analysis
+# Note: the plots generated from this don't need to be shown, it's best to hide
+# the figure when converting to RMarkdown
 set.seed(20160730)
 res <- poppr(pinf, sample = 999, quiet = TRUE,
              H = FALSE, G = FALSE, lambda = FALSE, E5 = FALSE,
@@ -20,12 +24,14 @@ set.seed(20160730)
 res_cc <- poppr(pinf, sample = 999, clonecorrect = TRUE, quiet = TRUE,
              H = FALSE, G = FALSE, lambda = FALSE, E5 = FALSE,
              strata = ~Continent/Country, keep = 1, total = FALSE)
-knitr::kable(res, digits = 2, caption = "Total Data Set")
-knitr::kable(res_cc, digits = 2, caption = "Clone Corrected Data")
+# knitr::kable(res, digits = 2, caption = "Total Data Set")
+write.table(res, file = "Total_Data.csv", row.names = FALSE, sep = ",")
+# knitr::kable(res_cc, digits = 2, caption = "Clone Corrected Data")
+write.table(res_cc, file = "Clone_Corrected_Data.csv", row.names = FALSE, sep = ",")
 p <- last_plot()
 op <- p # save the original plot
 
-## ----ia_plot-------------------------------------------------------------
+# ia plot
 p <- p + facet_wrap(~population, ncol = 1, scales = "free")
 p <- p + theme_bw() # initial theme
 p <- p + theme(text = element_text(size = rel(5))) # make everything bigger
@@ -39,7 +45,7 @@ p <- p + labs(title = "Index of Assocation for clone-corrected data")
 p <- p + labs(y = NULL)
 p
 
-## ----msn_plot------------------------------------------------------------
+# msn_plot
 setPop(pinf) <- ~Country
 min_span_net <- bruvo.msn(pinf, replen = pinfreps, add = TRUE, loss = FALSE,
                           showplot = FALSE, include.ties = TRUE)
@@ -55,6 +61,6 @@ plot_poppr_msn(pinf,
                quantiles = FALSE,
                beforecut = TRUE)
 
-## ----session_information-------------------------------------------------
+# session information
 devtools::session_info()
 
